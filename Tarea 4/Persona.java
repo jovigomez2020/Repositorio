ae26a349661fd9d84ab43fc49920341cf87563d3
jovigomez2020 <68304452+jovigomez2020@users.noreply.github.com>
2020-07-16 12:57:29 -0600

    Tarea 4

---
+++ Tarea 4/Persona.java
@@ -0,0 +1,19 @@
+
+//creando clase padre abstracta
+public abstract class Persona {
+    //declarando variables
+    private String nombre;
+    private int edad;
+    private String profesion;
+    
+    //metodos abstractos
+    public abstract String getNombre();
+    public abstract int getEdad();
+    public abstract String getprofesion();
+    
+    
+  
+    
+    
+    
+}
